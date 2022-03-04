9bb3a4274b66b623450aaa1ae456211a83f3a607
saite <saitejasai57@gmail.com>
2022-03-04 14:39:36 +0530

    first commit

---
+++ src/main/java/com/springbuildingblocks/springbootbuildingblocks/SpringbootBuildingBlocksApplication.java
@@ -0,0 +1,13 @@
+package com.springbuildingblocks.springbootbuildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingBlocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingBlocksApplication.class, args);
+	}
+
+}
